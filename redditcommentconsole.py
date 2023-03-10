8bf086204146ecb90ed8efe6bbd7288851a40390
rkojo <rikukojo1@gmail.com>
2023-03-10 23:36:18 +1100

    Added bypass to restricted accounts

--- redditcommentconsole.py
+++ redditcommentconsole.py
@@ -9,7 +9,20 @@ def openURL(browser, user):
   url = "https://old.reddit.com/user/" + user
   page = browser.open(url)
   print(browser.url)
+  if browser.url != url:
+     checkrestriction(browser, page)
+     page = browser.open(url)
   return page
+
+
+def checkrestriction(browser, page):
+      soup = BeautifulSoup(page.content, "html.parser")
+      but = soup.find("button", value  = "yes", class_ = "c-btn c-btn-primary")
+      fm = soup.find("form", class_ = "pretty-form")
+      form = browser.select_form(fm)
+      form.choose_submit(but)
+      browser.submit_selected()
+      return browser
 
 #finds all comments on the users first page 
 def parseAccount(page):
@@ -39,8 +52,11 @@ def nextpage(browser):
   arr = []
   try:
     while(browser.find_link(rel = "nofollow next")):
+      count = 2
+      print("Found page " + count)
       next = browser.follow_link(rel = "nofollow next")
       arr = arr + parseAccount(next)
+      count = count + 1
   except:
       print("finished finding all words")
   return arr
@@ -52,6 +68,7 @@ def plotcomments(arr):
   print(newpd)
   newpd.plot(x = "Word", y = 'Amount', kind='bar')
   plt.show()
+  
   # figure_canvas_agg = FigureCanvasTkAgg(plt.csd,canvas)
   # figure_canvas_agg.draw()
   # figure_canvas_agg.get_tk_widget().pack(expand=1)
